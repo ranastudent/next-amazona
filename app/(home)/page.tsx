85f990ce79054f250a8eac2b5cc17308103998b3
Rana <reduanulislam92665@gmail.com>
2025-02-27 23:27:21 +0600

    home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,68 @@
 import { HomeCarousel } from "@/components/shared/home/home-carousel";
+import { HomeCard } from "@/components/shared/home/home-card";
+import {
+  getAllCategories,
+  getProductsForCard,
+} from "@/lib/actions/product-actions";
 import data from "@/lib/data";
+import { Card, CardContent } from "@/components/ui/card";
+import { toSlug } from "@/lib/utils";
 
 export default async function page() {
-    
-      return <HomeCarousel items= {data.carousels} />
-  
+  const categories = (await getAllCategories()).slice(0, 4);
+  const newArrivals = await getProductsForCard({
+    tag: "new-arrival",
+  });
+  const featureds = await getProductsForCard({
+    tag: "featured",
+  });
+  const bestSellers = await getProductsForCard({
+    tag: "best-seller",
+  });
+  const cards = [
+      {
+        title: ('Categories to explore'),
+        link: {
+          text: ('See More'),
+          href: '/search',
+        },
+        items: categories.map((category) => ({
+          name: category,
+          image: `/images/${toSlug(category)}.jpg`,
+          href: `/search?category=${category}`,
+        })),
+      },
+      {
+        title: ('Explore New Arrivals'),
+        items: newArrivals,
+        link: {
+          text: ('View All'),
+          href: '/search?tag=new-arrival',
+        },
+      },
+      {
+        title: ('Discover Best Sellers'),
+        items: bestSellers,
+        link: {
+          text: ('View All'),
+          href: '/search?tag=new-arrival',
+        },
+      },
+      {
+        title: ('Featured Products'),
+        items: featureds,
+        link: {
+          text: ('Shop Now'),
+          href: '/search?tag=new-arrival',
+        },
+      },
+    ]
+  return (
+    <>
+      <HomeCarousel items={data.carousels} />
+      <div className='md:p-4 md:space-y-4 bg-border'>
+        <HomeCard cards={cards} />
+      </div>
+    </>
+  );
 }
