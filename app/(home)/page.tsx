4eb441f04c2217d1945bdfbfbbfd6437c31dbd73
Rana <reduanulislam92665@gmail.com>
2025-02-28 21:59:58 +0600

    browsing history done

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,6 +9,7 @@ import data from "@/lib/data";
 import { toSlug } from "@/lib/utils";
 import { Card, CardContent } from "@/components/ui/card";
 import ProductSlider from "@/components/shared/product/product-slider";
+import BrowsingHistoryList from "@/components/shared/browsing-history-list";
 
 export default async function page() {
   const categories = (await getAllCategories()).slice(0, 4);
@@ -82,6 +83,9 @@ export default async function page() {
             />
           </CardContent>
         </Card>
+      </div>
+      <div className='p-4 bg-background'>
+        <BrowsingHistoryList />
       </div>
     </>
   );
